1ed7a36137c54e4a13262ef32f69e28172d99f1b
finecalex <nechaev.finec@gmail.com>
2015-10-12 16:21:26 +0300

    Первый код

--- ConsoleApplication5/Program.cs
+++ ConsoleApplication5/Program.cs
@@ -9,13 +9,16 @@ class Program
 {
 static void Main(string[] args)
 {
+
 Console.WriteLine("==========================================");
 Console.WriteLine();
 Console.WriteLine("ДОБРО ПОЖАЛОВАТЬ! Вас приветствует программа по нахождению коэффициента финансовой независимости. РАЗВИВАЙТЕСЬ И БУДЬТЕ ФИНАНСОВО НЕ-ЗА-ВИ-СИ-МЫ!");
 Console.WriteLine();
 Console.WriteLine("==========================================");
-double a, b, c;
-Console.WriteLine("Введите количество собственные средств");
+double a, b, c, d;
+Console.WriteLine("Какой вариант развития у вашей компании? 1=традиционный; 0=стартап");
+d = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите количество собственны][ средств");
 a = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите количество заемных средств");
 b = Convert.ToDouble(Console.ReadLine());
